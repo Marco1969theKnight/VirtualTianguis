cb7a14608baed37e4cf027eb72c973cf0269b842
Marco Antonio Diaz Valdes <44275048+Marco1969theKnight@users.noreply.github.com>
2019-05-07 12:11:30 -0500

    Script creacion de base de datos en SQL Server
    En este script se crea la base de datos "VirTian" junto con las tablas "Clientes", "Factura", "Ventas", "DetalleVentas", "Empleado", "Producto", "Inventario", "Relacion_Proveedor_Producto" y "Proveedor". En casi todas las tablas tienen un Primary Key de sus identificadores y no contienen ninguna relación puesto que se crearan las relaciones después en un proyecto de Visual Studio, solo son modelos.

---
+++ QueryDeProyectoFinal.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE VirTian
+USE VirTian
+
+CREATE TABLE Clientes (
+	ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	RFC NVARCHAR(18) NOT NULL,
+	Nombre NVARCHAR(50),
+	APaterno NVARCHAR(50),
+	AMaterno NVARCHAR(50),
+	Estado NVARCHAR(50),
+	Ciudad NVARCHAR(50),
+	Calle NVARCHAR(50),
+	NoExt INT,
+	NoInt INT,
+	Colonia NVARCHAR(50),
+	CodigoPostal INT
+);
+
+CREATE TABLE Factura (
+	ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	FechaFacturacion DATE,
+	UsuarioEmisor NVARCHAR(50),
+	Pago MONEY,
+	Productos INT,
+	NoVenta INT NOT NULL,
+	Cliente INT NOT NULL,
+
+)
+
+CREATE TABLE Ventas (
+	NoVenta INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Fecha DATE,
+	Total INT,
+	Empleado INT NOT NULL,
+	Detalle INT  NOT NULL
+)
+
+CREATE TABLE DetalleVentas (
+	IdDetalleVenta INT NOT NULL IDENTITY(1,1) primary key,
+	Producto int not null
+)
+
+create table Empleado(
+	Id_empleado int not null IDENTITY(1,1) primary key,
+	Nombre nvarchar(50),
+	Apaterno nvarchar(50),
+	Amaterno nvarchar(50),
+	Nombre_usuario nvarchar(50),
+	Edad int,
+	Oficio nvarchar(50),
+	--Puesto int /*foreign key*/,
+	Editar BIT,
+	Almacen BIT,
+	Vender BIT,
+	Fecha_alta date,
+	Salario money,
+	Contraseña nvarchar(50)
+)
+
+create table Producto(
+	Id_producto int not null IDENTITY(1,1) primary key,
+	Nombre nvarchar(50),
+	Marca nvarchar(50),
+	Precio money,
+	--No_exist int, 
+	Categoria nvarchar(50),
+	Descripcion ntext,
+	Proveedor int not null
+)
+
+create table Inventario(
+	Productos int not null IDENTITY(1,1) primary key,
+	NumeroExistencias int
+)
+
+create table Relacion_Proveedor_Producto(
+	Proveedor int not null,
+	Producto int not null
+)
+
+create table Proveedor(
+	Id_proveedor int not null IDENTITY(1,1) primary key,
+	Nombre nvarchar(50)
+)
