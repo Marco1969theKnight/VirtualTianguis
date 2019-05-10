d3b0ca0093b36ddae1646dc63961c64e59e52cd9
Marco Antonio Diaz Valdes <44275048+Marco1969theKnight@users.noreply.github.com>
2019-05-10 16:24:42 -0500

    Carpeta Imagenes y forms
    Creacion de carpeta imagenes y de la carpeta forms

---
+++ TheVirtualTianguisProyect/TheVirtualTianguisProyect/Formas/FormEmpleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheVirtualTianguisProyect.Formas
+{
+    public partial class FormEmpleado : Form
+    {
+        public FormEmpleado()
+        {
+            InitializeComponent();
+        }
+    }
+}
