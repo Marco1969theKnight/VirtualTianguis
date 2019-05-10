744ef57ff45e10f8988f1d11ccc9e6cd7290db83
Marco Antonio Diaz Valdes <44275048+Marco1969theKnight@users.noreply.github.com>
2019-05-10 16:30:19 -0500

    creacion de carpeta forms

--- TheVirtualTianguisProyect/TheVirtualTianguisProyect/Formas/FormEmpleado.Designer.cs
+++ TheVirtualTianguisProyect/TheVirtualTianguisProyect/Formas/FormEmpleado.Designer.cs
@@ -28,19 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormEmpleado));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.AgregarButton = new System.Windows.Forms.ToolStripButton();
+            this.ModifButton = new System.Windows.Forms.ToolStripButton();
+            this.ElmButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AgregarButton,
+            this.ModifButton,
+            this.ElmButton,
+            this.toolStripButton4,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // AgregarButton
+            // 
+            this.AgregarButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.AgregarButton.Image = ((System.Drawing.Image)(resources.GetObject("AgregarButton.Image")));
+            this.AgregarButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.AgregarButton.Name = "AgregarButton";
+            this.AgregarButton.Size = new System.Drawing.Size(24, 24);
+            this.AgregarButton.Text = "Agregar";
+            // 
+            // ModifButton
+            // 
+            this.ModifButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ModifButton.Image = ((System.Drawing.Image)(resources.GetObject("ModifButton.Image")));
+            this.ModifButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ModifButton.Name = "ModifButton";
+            this.ModifButton.Size = new System.Drawing.Size(24, 24);
+            this.ModifButton.Text = "Modificar";
+            // 
+            // ElmButton
+            // 
+            this.ElmButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ElmButton.Image = ((System.Drawing.Image)(resources.GetObject("ElmButton.Image")));
+            this.ElmButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ElmButton.Name = "ElmButton";
+            this.ElmButton.Size = new System.Drawing.Size(24, 24);
+            this.ElmButton.Text = "Eliminar";
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton4.Text = "toolStripButton4";
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton5.Text = "toolStripButton5";
             // 
             // FormEmpleado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "FormEmpleado";
             this.Text = "Editar Empleados";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton AgregarButton;
+        private System.Windows.Forms.ToolStripButton ModifButton;
+        private System.Windows.Forms.ToolStripButton ElmButton;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
     }
 }
