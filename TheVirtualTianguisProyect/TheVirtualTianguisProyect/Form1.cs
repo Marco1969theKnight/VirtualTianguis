6473eecb2a38cd6815e5ceb50f8dbc0a18478e61
Luis Soriano <49929267+Laele@users.noreply.github.com>
2019-05-16 18:52:52 -0500

    Login distingue tipos de Inicio sesion

--- TheVirtualTianguisProyect/TheVirtualTianguisProyect/Form1.cs
+++ TheVirtualTianguisProyect/TheVirtualTianguisProyect/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+using System.Configuration;
+
 namespace TheVirtualTianguisProyect
 {
     public partial class VirtualTianguis : Form
@@ -25,7 +28,76 @@ namespace TheVirtualTianguisProyect
 
         private void button1_Click_1(object sender, EventArgs e)
         {
+            logins();
+        }
+
+        private void VirtualTianguis_Load(object sender, EventArgs e)
+        {
 
         }
+
+        public void logins()
+        {
+            try
+            {
+                //string cnn = ConfigurationManager.ConnectionStrings["TheVirtualTianguisProyect.Properties.Settings.Conexion"].ConnectionString;
+                string cnn = ConfigurationManager.ConnectionStrings["TheVirtualTianguisProyect.Properties.Settings.Conexion"].ConnectionString;
+                using (SqlConnection conexion = new SqlConnection(cnn))
+                {
+                    conexion.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT Nombre_usuario, Contraseña, Editar, Almacen, Vender FROM Empleado WHERE Nombre_usuario ='" + NomUsuTB.Text + "'AND Contraseña='" + ConTB.Text + "'", conexion))
+                    {
+                        SqlDataReader dr = cmd.ExecuteReader();
+
+                        if (dr.Read())
+                        {
+                            Boolean Editar = dr.GetBoolean(2);
+                            Boolean Almacen = dr.GetBoolean(3);
+                            Boolean Vender = dr.GetBoolean(4);
+
+
+                            {
+                                if (Editar && Vender && !Almacen)
+                                {
+                                    MessageBox.Show("Login Vendedor");
+
+                                }
+
+
+                                if (Almacen && Vender && !Editar)
+                                {
+                                    MessageBox.Show("Login Supervisor");
+                                }
+
+
+                                if (Editar && Vender && Almacen)
+                                {
+                                    MessageBox.Show("Login Gerente");
+                                }
+
+
+                                if (!(Editar && Vender && Almacen))
+                                {
+                                    MessageBox.Show("Login Otros");
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Datos incorrectos");
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
     }
 }
