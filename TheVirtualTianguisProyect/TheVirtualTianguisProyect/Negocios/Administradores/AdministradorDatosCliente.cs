87d6ad1296f2915059c2f42e629b5ad70bd7e20c
Marco Antonio Diaz Valdes <44275048+Marco1969theKnight@users.noreply.github.com>
2019-05-23 11:44:57 -0500

    Corregi error

--- TheVirtualTianguisProyect/TheVirtualTianguisProyect/Negocios/Administradores/AdministradorDatosCliente.cs
+++ TheVirtualTianguisProyect/TheVirtualTianguisProyect/Negocios/Administradores/AdministradorDatosCliente.cs
@@ -94,7 +94,7 @@ namespace TheVirtualTianguisProyect.Negocios.Administradores
         public static void ActualizaDatosCliente(TheVirtualTianguisProyect.Negocios.Datos.DatosCliente DatosUsuario)
         {
             String ConnectionString = String.Format(TheVirtualTianguisProyect.Properties.Settings.Default.Conexion);
-            String query = "update Clientes set RFC = " + "'" + DatosUsuario.RFC + "', Nombre = " + "'" + DatosUsuario.Nombre + "', APaterno = " + "'" + DatosUsuario.APaterno + "', AMaterno = " + "'" + DatosUsuario.AMaterno + "', Estado = " + "'" + DatosUsuario.Estado + "', Ciudad = " + "'" + DatosUsuario.Ciudad + "', Calle = " + "'" + DatosUsuario.Calle + "', NoExt = " + DatosUsuario.NoExt + ", " + ", NoInt = " + DatosUsuario.NoInt + ", Colonia = " + "'" + DatosUsuario.Colonia + "', CodigoPostal = " + DatosUsuario.CodigoPostal + ", ClienteActivo = " + DatosUsuario.ClienteActivo + "' where ID = " + DatosUsuario.ID;
+            String query = "update Clientes set RFC = " + "'" + DatosUsuario.RFC + "', Nombre = " + "'" + DatosUsuario.Nombre + "', APaterno = " + "'" + DatosUsuario.APaterno + "', AMaterno = " + "'" + DatosUsuario.AMaterno + "', Estado = " + "'" + DatosUsuario.Estado + "', Ciudad = " + "'" + DatosUsuario.Ciudad + "', Calle = " + "'" + DatosUsuario.Calle + "', NoExt = " + DatosUsuario.NoExt + ", NoInt = " + DatosUsuario.NoInt + ", Colonia = " + "'" + DatosUsuario.Colonia + "', CodigoPostal = " + DatosUsuario.CodigoPostal + ", ClienteActivo = " + DatosUsuario.ClienteActivo + "' where ID = " + DatosUsuario.ID;
 
             TheVirtualTianguisProyect.Persistencia.AdministradorDatos.ExecuteNonQuery(ConnectionString, query);
         }
